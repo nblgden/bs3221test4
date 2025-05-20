e4002322300eabb97d5b0837e911e136ef09bc38
nblgden <n.blagden.22@unimail.winchester.ac.uk>
2025-05-20 12:25:37 +0100

    Update EditWardenModal.jsx
    add location list to edit page

--- src/components/EditWardenModal.jsx
+++ src/components/EditWardenModal.jsx
@@ -16,6 +16,18 @@ export default function EditWardenModal({ warden, onClose, onSave }) {
       [name]: value
     }));
   };
+  
+  const locations = [
+    "Alwyn Hall", "Beech Glade", "Bowers Building", "Burma Road Student Village",
+    "Centre for Sport", "Chapel", "The Cottage", "Fred Wheeler Building",
+    "Herbert Jarman Building", "Holm Lodge", "Kenneth Kettle Building",
+    "King Alfred Centre", "Martial Rose Library", "Masters Lodge", "Medecroft",
+    "Medecroft Annexe", "Paul Chamberlain Building", "Queen's Road Student Village",
+    "St Alphege", "St Edburga", "St Elizabeth's Hall", "St Grimbald's Court",
+    "St James' Hall", "St Swithun's Lodge", "The Stripe", "Business School",
+    "Tom Atkinson Building", "West Downs Centre", "West Downs Student Village",
+    "Winton Building", "Students' Union"
+  ];
 
   const handleSubmit = (e) => {
     e.preventDefault();
